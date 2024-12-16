7b968d28a52dd3fea190ce949d25c6d892a8605a
Leo Zhao <leo.zhao@intel.com>
2024-12-16 07:15:35 +0000

    integrate all fused ops, first fused op still need to optimize on embedding.

--- paddlenlp/experimental/transformers/llama/modeling.py
+++ paddlenlp/experimental/transformers/llama/modeling.py
@@ -92,7 +92,7 @@ class FusedLlamaRMSNorm(nn.Layer):
 
     def forward(self, hidden_states):
         result = paddle.incubate.nn.functional.fused_rms_norm(
-            hidden_states, self.weight, None, self.variance_epsilon, begin_norm_axis=1
+            hidden_states, self.weight, None, self.variance_epsilon, begin_norm_axis=2
         )
         if isinstance(result, tuple):
             return result[0]
